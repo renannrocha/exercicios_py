1edbbce2bec25626e4e8e35b4ea0636ba38ef562
therenan8897 <renanrocha8897@gmail.com>
2022-10-21 19:53:57 -0300

    first commit

--- ex001.py
+++
@@ -1,5 +0,0 @@
-# python  modulo 1  curso em video
-# ex 001 - Respondendo ao Usuario
-
-nome = (input('digite seu nome:'))
-print(f'ola {nome} , é um prazer lhe conhecer !')
---
+++ exercicios.py/ex001.py
@@ -0,0 +1,4 @@
+# exercicio 001 - Respondendo o usuario 
+
+nome = input('digite o seu nome')
+print(f'Ola {nome}, e um prazer lhe conhecer ')
