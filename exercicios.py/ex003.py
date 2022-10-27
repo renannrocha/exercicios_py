5dc7bf947e962b670039da1f332a4d4068117d42
therenan8897 <renanrocha8897@gmail.com>
2022-10-26 22:59:16 -0300

    add exercicio

--- exercicios.py/ex003.py
+++ exercicios.py/ex003.py
@@ -1,2 +1,13 @@
 # exercicio 003 - Dissecando uma variável
 
+dado = input('digite algo ')
+print (f'{dado} é numerico : {dado.isnumeric()}')
+print (f'{dado} é uma letra : {dado.isalpha()}')
+print (f'{dado} é alfanumerico : {dado.isalnum()}')
+print (f'{dado} esta só com letras maiusculas :{dado.isupper()}')
+print (f'{dado} esta só com letras minusculas :{dado.islower()}')
+print (f'{dado} é um titulo :{dado.istitle()}')
+print (f'{dado} é um espaço :{dado.isspace()}')
+print (f'{dado} é um digito :{dado.isdigit()}')
+print (f'{dado} é um numero decimal :{dado.isdecimal()}')
+print (f'{dado} é um identificador :{dado.isidentifier()}')
